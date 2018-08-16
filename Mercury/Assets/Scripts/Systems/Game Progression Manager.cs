748eb4be547d96bf658f7661963286253c4cc4f7
Johan Pieterse <ja.pieterse3@gmail.com>
2018-08-16 14:00:38 +0200

    Basic Progression Class

---
+++ Mercury/Assets/Scripts/Systems/Game Progression Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProgressionManager : MonoBehaviour {
+
+    private int numEnemiesLeft;
+    private int numOfBulletsUsed;
+    private int enemiesKilled;
+    private int wallsDestroyed;
+    private int damageTaken;
+    private Time currentStageTime;
+    private Time totalTimePlayed;
+    private GameObject levels;
+    private GameObject[] players;
+    private GameObject[] gunsUsed;
+
+    // The Game progression manager is a singleton
+    public static GameProgressionManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
