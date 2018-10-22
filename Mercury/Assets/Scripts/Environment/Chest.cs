49ef8bf735cf7b67f36fd815816a88c3a6641b8b
Stoofie2 <41577992+Stoofie2@users.noreply.github.com>
2018-10-22 08:53:53 +0200

    Fixed chest bug and improved inheritance

--- Mercury/Assets/Scripts/Environment/Chest.cs
+++ Mercury/Assets/Scripts/Environment/Chest.cs
@@ -8,6 +8,7 @@ public class Chest : MonoBehaviour
     public float destroy =5f;
     protected Transform visual;
     private Vector3 targetPos;
+    int count = 1;
 
     private void Awake()
     {
@@ -25,11 +26,130 @@ public class Chest : MonoBehaviour
 
     protected virtual void Use()
     {
+        if (count >= 1)
+        {
+            count--;
 
+            GameObject[] chestObjects = GetRandomObjects();
+
+            for (int i = 0; i < chestObjects.Length; i++)
+            {
+                chestObjects[i].transform.position = transform.position + new Vector3(UnityEngine.Random.Range(-1f, 1f), 0.5f, UnityEngine.Random.Range(-1f, 1f));
+                chestObjects[i].GetComponent<Rigidbody>().AddForce(Vector3.up * 5 + UnityEngine.Random.onUnitSphere * 2, ForceMode.Impulse);
+            }
+        }
     }
 
     protected virtual void Destroy()
     {
        Destroy(gameObject);
+    }
+
+
+    protected virtual GameObject[] GetRandomObjects()
+    {
+        int objectCount = 1;
+
+        if (UnityEngine.Random.Range(0, 100) > 30)
+        {
+            objectCount += 1;
+        }
+        else
+        if (UnityEngine.Random.Range(0, 100) > 50)
+        {
+            objectCount += 2;
+        }
+        else
+        if (UnityEngine.Random.Range(0, 100) > 80)
+        {
+            objectCount += 3;
+        }
+
+        List<GameObject> objectList = new List<GameObject>();
+        for (int i = 0; i < objectCount; i++)
+        {
+            objectList.Add(GetRandomItem());
+        }
+
+        return objectList.ToArray();
+    }
+
+    protected virtual GameObject GetRandomItem()
+    {
+        int[] itemWeights = {
+            60, 30, 30, 30, 20, 30,
+            50, 50, 50, 50, 50};
+
+        int total = 0;
+        for (int i = 0; i < itemWeights.Length; i++)
+        {
+            total += itemWeights[i];
+        }
+
+        int random = UnityEngine.Random.Range(0, total);
+
+        total = 0;
+        int result = 0;
+        for (int i = 0; i < itemWeights.Length; i++)
+        {
+            total += itemWeights[i];
+            if (random >= total)
+            {
+                result = i;
+                break;
+            }
+        }
+        //Weapons.
+        if (result == 0)
+        {
+            return Factory.instance.CreatePistol();
+        }
+        if (result == 1)
+        {
+            return Factory.instance.CreateMachineGun();
+        }
+        if (result == 2)
+        {
+            return Factory.instance.CreateSniperRifle();
+        }
+        if (result == 3)
+        {
+            return Factory.instance.CreateRocketLauncher();
+        }
+        if (result == 4)
+        {
+            return Factory.instance.CreateShotgun();
+        }
+        if (result == 5)
+        {
+            return Factory.instance.CreateLaserPistol();
+        }
+        if (result == 6)
+        {
+            return Factory.instance.CreateSword();
+        }
+
+        // Consumables
+        if (result == 7)
+        {
+            return Factory.instance.CreateMedkit();
+        }
+        if (result == 8)
+        {
+            return Factory.instance.CreateMedpack();
+        }
+        if (result == 9)
+        {
+            return Factory.instance.CreateBeamAmmoPack();
+        }
+        if (result == 10)
+        {
+            return Factory.instance.CreateBulletAmmoPack();
+        }
+        if (result == 11)
+        {
+            return Factory.instance.CreateRocketAmmoPack();
+        }
+        return null;
     }
 }
