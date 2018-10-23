5797981c9de34cfe697f931044b1168612457189
Stoofie2 <41577992+Stoofie2@users.noreply.github.com>
2018-10-23 12:09:46 +0200

    Fixed burst rifle

--- Mercury/Assets/Scripts/Weapons/BurstAssaultRifle.cs
+++ Mercury/Assets/Scripts/Weapons/BurstAssaultRifle.cs
@@ -19,6 +19,21 @@ public class BurstAssaultRifle : WeaponRanged
         damage = 20f;
     }
 
+    IEnumerator BulletWaitTime()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject bullet = Factory.instance.CreateBullet();
+            bullet.GetComponent<Round>().setDamage(damage);
+            bullet.GetComponent<Projectile>().speed *= 1;
+            bullet.transform.position = transform.position + transform.right * ammoOffset;
+            bullet.transform.right = transform.right;
+            bullet.transform.localEulerAngles += new Vector3(0, Random.Range(-ammoRandomness, ammoRandomness), 0);
+            bullet.GetComponent<Projectile>().Update();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
     protected override void Use()
     {
 
@@ -27,31 +42,9 @@ public class BurstAssaultRifle : WeaponRanged
         GameObject flash = Factory.instance.CreateMuzzleFlash();
         flash.transform.position = transform.position + transform.right * ammoOffset;
         Destroy(flash, 1);
+        StartCoroutine(BulletWaitTime());
 
 
-        GameObject bullet = Factory.instance.CreateBullet();
-        bullet.GetComponent<Round>().setDamage(damage);
-        bullet.GetComponent<Projectile>().speed *= 2;
-        bullet.transform.position = transform.position + transform.right * ammoOffset;
-        bullet.transform.right = transform.right;
-        bullet.transform.localEulerAngles += new Vector3(0, Random.Range(-ammoRandomness, ammoRandomness), 0);
-        bullet.GetComponent<Projectile>().Update();
-
-        GameObject bullet2 = Factory.instance.CreateBullet();
-        bullet2.GetComponent<Round>().setDamage(damage);
-        bullet2.GetComponent<Projectile>().speed *= 1.75f;
-        bullet2.transform.position = transform.position + transform.right * ammoOffset;
-        bullet2.transform.right = transform.right;
-        bullet2.transform.localEulerAngles += new Vector3(0, Random.Range(-ammoRandomness, ammoRandomness), 0);
-        bullet2.GetComponent<Projectile>().Update();
-
-        GameObject bullet3 = Factory.instance.CreateBullet();
-        bullet3.GetComponent<Round>().setDamage(damage);
-        bullet3.GetComponent<Projectile>().speed *= 1.5f;
-        bullet3.transform.position = transform.position + transform.right * ammoOffset;
-        bullet3.transform.right = transform.right;
-        bullet3.transform.localEulerAngles += new Vector3(0, Random.Range(-ammoRandomness, ammoRandomness), 0);
-        bullet3.GetComponent<Projectile>().Update();
         AudioManager.instance.PlayAudio("BurstAR_sound_effect", 1, false);
 
         CameraSystem.instance.ShakePosition(-transform.right * 0.2f);
