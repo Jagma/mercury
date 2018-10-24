729bf82a9db6a39b3bb21c3adbcf95321aaf5f53
Stoofie2 <41577992+Stoofie2@users.noreply.github.com>
2018-10-24 10:03:37 +0200

    Reworked chests to spawn specific items on specific levels

--- Mercury/Assets/Scripts/Environment/NormalChest.cs
+++ Mercury/Assets/Scripts/Environment/NormalChest.cs
@@ -7,55 +7,40 @@ public class NormalChest : Chest
 
     protected override GameObject GetRandomItem()
     {
-        int result = Random.Range(0, 10);
-        //Weapons.
-        if (result == 0)
+        int result;
+        if (ProgressionState.environmentName == "Mars")
         {
-            return Factory.instance.CreatePistol();
-        }
-        if (result == 1)
-        {
-            return Factory.instance.CreateMachineGun();
-        }
-        if (result == 2)
-        {
-            return Factory.instance.CreateSniperRifle();
-        }
-        if (result == 3)
-        {
-            return Factory.instance.CreateRocketLauncher();
-        }
-        if (result == 4)
-        {
-            return Factory.instance.CreateShotgun();
-        }
-        if (result == 5)
-        {
-            return Factory.instance.CreateLaserPistol();
-        }
-        if (result == 6)
-        {
-            return Factory.instance.CreateSword();
+            result = Random.Range(0, 2);
+            if (result == 0)
+                return Factory.instance.CreatePistol();
+            if (result == 1)
+                return Factory.instance.CreateMachineGun();
+            if (result == 2)
+                return Factory.instance.CreateSniperRifle();
         }
 
-        if (result == 7)
+        if (ProgressionState.environmentName == "Venus")
         {
-            return Factory.instance.CreateStrongAxe();
+            result = Random.Range(3, 6);
+            if (result == 3)
+                return Factory.instance.CreateRocketLauncher();
+            if (result == 4)
+                return Factory.instance.CreateShotgun();
+            if (result == 6)
+                return Factory.instance.CreateSword();
         }
 
-        if (result == 8)
+        if (ProgressionState.environmentName == "Mercury")
         {
-            return Factory.instance.CreateWeakAxe();
+            result = Random.Range(5, 8);
+            if (result == 5)
+                return Factory.instance.CreateLaserPistol();
+            if (result == 7)
+                return Factory.instance.CreateStrongAxe();
+            if (result == 8)
+                return Factory.instance.CreateWeakAxe();
         }
-        // Consumables
-        if (result == 9)
-        {
-            return Factory.instance.CreateMedkit();
-        }
-        if (result == 10)
-        {
-            return Factory.instance.CreateMedpack();
-        }
+      
         return null;
     }
 }
